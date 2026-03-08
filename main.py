f056317984dc460d32f7fdc6fb728e014f4d07e3
Joel Cutler <joelcutler108@gmail.com>
2026-03-08 23:25:21 +0000

    add try/catch

--- main.py
+++ main.py
@@ -30,27 +30,30 @@ def main():
         context = browser.new_context()
         page = context.new_page()
 
-        # Navigate to iShopForIpsos
-        page.goto("https://uk.ishopforipsos.com/next/fieldportal/")
+        try:
+            # Navigate to iShopForIpsos
+            page.goto("https://uk.ishopforipsos.com/next/fieldportal/")
 
-        page.wait_for_load_state(state="domcontentloaded")
+            # Wait for the page to load
+            # ? Waiting until there are no network connections is discouraged
+            page.wait_for_load_state(state="networkidle")
 
-        # Enter credentials and login
-        email = require_env("IPSOS_EMAIL")
-        page.fill("#email", email)
-        password = require_env("IPSOS_PASSW")
-        page.fill("#password", password)
-        page.click("#btn-login")
+            # Enter credentials and login
+            email = require_env("IPSOS_EMAIL")
+            page.fill("#email", email)
+            password = require_env("IPSOS_PASSW")
+            page.fill("#password", password)
+            page.click("#btn-login")
 
-        # Click side panel navigator to switch to Task List
-        page.click('text="Task Board (List)"')
+            # Click side panel navigator to switch to Task List
+            page.click('text="Task Board (List)"')
 
-        # Wait for the results to load
-        # ? Waiting until there are no network connections is discouraged
-        page.wait_for_load_state(state="networkidle")
+            # Wait for the results to load
+            # ? Waiting until there are no network connections is discouraged
+            page.wait_for_load_state(state="networkidle")
 
-        # Get the iframe, the results are stored in
-        frame = page.frame_locator("#appContentFrame")
+            # Get the iframe, the results are stored in
+            frame = page.frame_locator("#appContentFrame")
 
             # Search for given postcode within the results
             postcode_count = frame.get_by_text(postcode).count()
