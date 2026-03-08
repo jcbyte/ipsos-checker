9a9e489caa3672b4622261c26087fd62fb7ae63b
Joel Cutler <joelcutler108@gmail.com>
2026-03-08 14:58:46 +0000

    access environment variables

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import os
+
+from dotenv import load_dotenv
 from playwright.sync_api import sync_playwright
+
+
+def require_env(name: str) -> str:
+    value = os.getenv(name)
+    if value is None:
+        raise KeyError(f"{name} environment variable is not set")
+
+    return value
 
 
 def main():
@@ -10,9 +21,15 @@ def main():
         print("1: Navigating to iShopForIpsos")
         page.goto("https://uk.ishopforipsos.com/next/fieldportal/")
 
+        email = require_env("IPSOS_EMAIL")
+        password = require_env("IPSOS_PASSW")
+        page.fill("#email", email)
+        page.fill("#password", password)
+
         input("AWAIT")
         browser.close()
 
 
 if __name__ == "__main__":
+    load_dotenv()
     main()
