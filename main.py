1133d2f41e7057cb108cc8538ad0bcd2860e1569
Joel Cutler <joelcutler108@gmail.com>
2026-03-08 14:49:48 +0000

    navigate to ipsos

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ from playwright.sync_api import sync_playwright
 
 
 def main():
-    print("Hello, World!")
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        print("1: Navigating to iShopForIpsos")
+        page.goto("https://uk.ishopforipsos.com/next/fieldportal/")
+
+        input("AWAIT")
+        browser.close()
 
 
 if __name__ == "__main__":
