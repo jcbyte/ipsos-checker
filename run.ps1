0c5008210028a31a12b63092eeb16ac3d5fdd353
Joel Cutler <joelcutler108@gmail.com>
2026-03-08 23:45:01 +0000

    add powershell script to run python file

---
+++ run.ps1
@@ -0,0 +1,23 @@
+# Execute `main.py`, creating an environment if one does not already exist
+
+$VenvPath = "$PSScriptRoot/.venv"
+$VenvPython = "$VenvPath/Scripts/python.exe"
+$PyScript = "$PSScriptRoot/main.py"
+
+# If the environment doesn't exist then create it
+if (-not (Test-Path $VenvPath)) {
+  # Create virtual environment
+  python -m venv $VenvPath
+
+  # Install packages
+  & $VenvPython -m pip install -r "$PSScriptRoot/requirements.txt"
+
+  # Install playwright browsers are installed
+  $VenvPlaywright = "$VenvPath/Scripts/playwright.exe"
+  & $VenvPlaywright install chromium
+
+  Write-Host "-----`n" -ForegroundColor Gray
+}
+
+# Execute the python script using the environment
+& $VenvPython $PyScript
